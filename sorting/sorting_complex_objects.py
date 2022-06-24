c5cd998f78f2326292c2bda7b46971646659a5a6
edewajosh <joshedewa@gmail.com>
2022-06-24 22:50:56 +0300

    itemgetter does not allow initialization using objects

--- sorting/sorting_complex_objects.py
+++ sorting/sorting_complex_objects.py
@@ -20,16 +20,24 @@ if __name__ == '__main__':
         Employee('John', 143, 95000),
         Employee('Jane', 193, 100000),
     ]
+
+    company = [
+        ('Dave', 173, 122000),
+        ('Dyke', 172, 122000),
+        ('Jake', 124, 142000),
+        ('John', 143, 95000),
+        ('Jane', 193, 100000),
+    ]
     # sorting by age alone in a ascending order
     sorted_employees = sorted(employees, key=lambda employee:employee.salary)
     # sorting by salary in a descending order
     sort_by_salary_desc = sorted(employees, key=lambda employee:employee.salary, reverse=True)
     # sorting by two attrs(salary & staff_no) by salary fast then staff_no
     multiple_level_sorting = sorted(employees, key=attrgetter('salary', 'staff_no'))
-    # multiple_level_sorting_2 = sorted(employees, key=itemgetter(1,2))
+    multiple_level_sorting_2 = sorted(company, key=itemgetter(1,2))
     # NB: Sorts are guaranteed to be stable. That means that when multiple records have 
     # the same key, their original order is preserved.
     print(f"Ascending order: {sorted_employees}")
     print(f"Descending order: {sort_by_salary_desc}")
     print(f"Multiple level sorting: { multiple_level_sorting}")
-    # print(f"Multiple level sorting 1: { multiple_level_sorting_2}")
+    print(f"Multiple level sorting 1: { multiple_level_sorting_2}")
