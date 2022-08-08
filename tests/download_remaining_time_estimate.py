61da12f5b052e00553542cf6e16099740bfaa3d8
edewajosh <joshedewa@gmail.com>
2022-08-08 15:48:31 +0300

    toptal codility algorithm

---
+++ tests/download_remaining_time_estimate.py
@@ -0,0 +1,25 @@
+import math
+
+class RemainingTime:
+
+    def __init__(self, file_size, file_size_downloaded, last_items):
+        self.file_size = file_size
+        self.file_size_downloaded = file_size_downloaded
+        self.last_items = last_items
+
+    def solution(self):
+        if sum(self.file_size_downloaded) == self.file_size:
+            return 0
+        if len(self.file_size_downloaded) < self.last_items:
+            average_of_last_items = sum(self.file_size_downloaded)/len(self.file_size_downloaded)
+            return math.ceil(self.file_size/average_of_last_items)
+
+        else:
+            average_of_last_items = sum(self.file_size_downloaded[-self.last_items:])/self.last_items
+            return math.ceil((self.file_size-sum(self.file_size_downloaded))/average_of_last_items)
+
+if __name__ == '__main__':
+    print("Downlaod time remaining estimate")
+    obj = RemainingTime(100, [10,6,6,8], 2)
+
+    print(obj.solution())
