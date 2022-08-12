a0443a2c5141c0281dfe0cca0f7c7c37e62f29c1
edewajosh <joshedewa@gmail.com>
2022-08-12 16:41:41 +0300

    0 value was returning false for isaphanum

--- two_pointers/valid_palindrome.py
+++ two_pointers/valid_palindrome.py
@@ -1,9 +1,46 @@
 class Solution:
+
+    """
+    A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+    Given a string s, return true if it is a palindrome, or false otherwise.
+
+    Example 1:
+
+    Input: s = "A man, a plan, a canal: Panama"
+    Output: true
+    Explanation: "amanaplanacanalpanama" is a palindrome.
+    Example 2:
+
+    Input: s = "race a car"
+    Output: false
+    Explanation: "raceacar" is not a palindrome.
+    Example 3:
+
+    Input: s = " "
+    Output: true
+    Explanation: s is an empty string "" after removing non-alphanumeric characters.
+    Since an empty string reads the same forward and backward, it is a palindrome.
+    
+
+    Constraints:
+
+    1 <= s.length <= 2 * 105
+    s consists only of printable ASCII characters.
+    """
     def isPalindrome(self, s: str) -> bool:
-        val = s.lower()
-        val = [i for i in val if not i.isdigit() and i.isalpha()]
+        if  len(s) == 0:
+            return True
+        val = []
+        for i in s:
+            if i.isdigit():
+                val.append(i)
+            elif i.isalpha():
+                val.append(i)
+            else:
+                pass
         res = ''.join(val)
-        if res[::-1] == res:
+        if res[::-1].lower() == res.lower():
             return True
         else:
             return False
